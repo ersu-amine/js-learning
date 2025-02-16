e3a048382bab090012a714ad45e919741af3a498
ersu-amine <ersu-amine@gmail.com>
2025-02-16 00:19:42 -0500

    practice day1

---
+++ day01/HelloWorld.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+
+console.log("Hello Hello");
+
+console.log("first day of js");
+
---
+++ day01/PracticeVariables.js
@@ -0,0 +1,26 @@
+// var, let, const keywords for variables
+
+//let more preferred
+
+//let name;
+//undefined before it has data
+
+let name = "Amine";
+
+console.log(name);
+
+let value = 20;
+console.log(typeof value);
+console.log(value);
+
+let variable = true;
+console.log(typeof variable);
+console.log(variable);
+
+//can have double type number
+value = 7.5;
+console.log(value);
+
+//var - global type, always reachable
+
+//let -> can be used for local variable purposes inside a block
