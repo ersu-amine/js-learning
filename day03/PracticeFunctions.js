dc6d8d2aa6b0c9ae55e55a28b4ca60e8028cdc0a
ersu-amine <ersu.amine@gmail.com>
2025-02-19 00:20:41 -0500

    practice functions

---
+++ day03/PracticeFunctions.js
@@ -0,0 +1,22 @@
+//repetable code fragment
+//function outside of class, mehtod inside of class
+
+//for reusability
+//independent of class like the method
+
+//create function
+//cannot declare the return type
+function displayNumber(){
+    console.log(3);
+}
+
+//call the function
+displayNumber();
+
+function login(username, password){
+    console.log(`User ${username} is trying to login with password ${password}`);
+    console.log(`User has logged in successfully`);
+    return true;
+}
+
+console.log(login("admin", "admin123"));
