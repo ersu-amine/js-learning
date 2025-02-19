60634cecfdef5f0a512b4cd52b76525b08e7d3e6
ersu-amine <ersu.amine@gmail.com>
2025-02-18 23:32:53 -0500

    practice string methods

--- day02/PracticeString.js
+++ day02/PracticeString.js
@@ -29,6 +29,25 @@ console.log(name2.toUpperCase() === name.toUpperCase()); //true
 console.log(name.includes("ne")); //true
 console.log(name.includes("me")); //false
 
-//replace method
+//replace method, replace the first occurance
 name2 = name2.replace("a", "");
+name2 = name2.replace("e", "i");
 console.log(name2); //mine
+
+//replace all occurances
+let str = "Banana";
+console.log(str); //Banana
+str = str.replaceAll("n","N");
+console.log(str);//BaNaNa
+
+
+//includes() method
+let emailAddress = "email@gmail.com";
+console.log(emailAddress.includes(".com")); //true
+
+//substring() method
+let domain = emailAddress.substring(emailAddress.indexOf("@")+1, emailAddress.indexOf(".")); //gmail
+
+//String concatenation
+let str1 = "Hello " + name; //Hello Amine, with plus operator concatenation
+let str2 = `Hello ${name}`; //Hello Amine, placeholder concatenation with ${}, must use backticks
